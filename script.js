f433f12b481a1d0f6cc2d7fcbfc5c043c32580e2
cissna <isaaccissna@gmail.com>
2024-02-01 16:49:31 -0800

    Finalized Energy Conversion Calculator (For now, at least it works)

--- script.js
+++ script.js
@@ -33,9 +33,9 @@ $(document).ready(function() {
                         '<td contenteditable="true" class="editableCell"></td>' +
                         '<td contenteditable="true" class="editableCell"></td>' +
                         `<td class="radio-cell">
-                            <input type="radio" name="options' + new_row_num + '" value="Not Peak">No
-                            <input type="radio" name="options' + new_row_num + '" value="Part Peak">Partly
-                            <input type="radio" name="options' + new_row_num + '" value="Peak">Yes
+                            <input type="radio" name="options' + new_row_num + '" value="No">No
+                            <input type="radio" name="options' + new_row_num + '" value="Partly">Partly
+                            <input type="radio" name="options' + new_row_num + '" value="Yes">Yes
                         </td>` +
                         '<td contenteditable="false" class="hoverable"></td>' +
                         '<td contenteditable="false" class="hoverable"></td>' +
@@ -49,7 +49,7 @@ $(document).ready(function() {
         let inputValue = $(this).text();
 
         // Remove non-numeric characters using a regular expression
-        let numericValue = inputValue.replace(/[^0-9]/g, '');
+        let numericValue = inputValue.replace(/[^0-9.]/g, '');
 
         // Check if the cleaned value is a valid number
         if (isNaN(numericValue)) {
@@ -89,13 +89,33 @@ $(document).ready(function() {
             const col2 = parseFloat($row.find("td:eq(2)").text()) || 0;
             const col3 = parseFloat($row.find("td:eq(3)").text()) || 0;
 
-            const sum = col2 + col3;
-            const prod = col2 * col3;
-            $row.find("td:eq(5)").text(Number(sum.toFixed(2)).toString());
-            $row.find("td:eq(6)").text(Number(prod.toFixed(2)).toString());
+            const radio_choice = $row.find("td:eq(4)").find('input[type="radio"]:checked').val();
+            let kWh_to_dollars;
+
+            switch (radio_choice) {
+                case "No": {
+                    kWh_to_dollars = .0516;
+                    break;
+                }
+                case "Yes": {
+                    kWh_to_dollars = 0.23945625;
+                    break;
+                }
+                case "Partly": {
+                    kWh_to_dollars = (0.23945625 + .0516) / 2;
+                    break;
+                }
+                default: {
+                    kWh_to_dollars = 0.1634;
+                }
+            }
+            const energy = col2 * col3 * 30.5/ 1000;
+            const dollars = energy * kWh_to_dollars;
+            $row.find("td:eq(5)").text(Number(energy.toFixed(2)).toString());
+            $row.find("td:eq(6)").text(Number(dollars.toFixed(2)).toString());
         });
         // figure out sum of columns 5 and 6. Save them somewhere to use in the hovering thing
-        updateSums()
+        updateSums();
         const resultElement = $(".result");
         resultElement.text('Total Monthly Cost: $' + Number(sumMoney.toFixed(2)).toString());
         resultElement.on("mouseenter", function() {
@@ -112,10 +132,10 @@ $(document).ready(function() {
         if (initialContent !== undefined && initialContent !== '') {
             const columnIndex = $(this).index();
             if (![5, 6].includes(columnIndex)) {
-                console.error('incorrect cell flagged as hoverable or indexes changed')
+                console.error('incorrect cell flagged as hoverable or indexes changed');
             }
-            updateSums()
-            const appropriateSum = columnIndex === 6? sumMoney: sumEnergy
+            updateSums();
+            const appropriateSum = columnIndex === 6? sumMoney: sumEnergy;
 
             // check if column sum is 0, if it is just say 0%
             if (appropriateSum === 0) {
@@ -123,7 +143,7 @@ $(document).ready(function() {
             } else {
                 $(this).text(
                     Number((Number(100 * initialContent) / appropriateSum).toFixed(2))
-                    + "%");
+                    + "%")
             }
             $(this).data("initialContent", initialContent);
         }

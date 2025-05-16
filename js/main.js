649cb5da5025b8d1a90be89e703cfb7ae4406614
Isaac Cissna <isaaccissna@gmail.com>
2025-05-16 02:48:10 -0700

    fixed bug where collectForm didn't get all checkbox items.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
-/* ---------- helpers ---------- */
-const STORAGE_KEY = 'chosenUser';   // 'isaac' or 'mira'
+const STORAGE_KEY = 'chosenUser';
 
-export function setUser(u){ localStorage.setItem(STORAGE_KEY, u); }
-export function getUser(){ return localStorage.getItem(STORAGE_KEY) || ''; }
-
-/* Build an object of the form inputs so you can do whatever you want */
-export function collectForm(elForm){
-  const data = Object.fromEntries(new FormData(elForm).entries());
-  data.user = getUser();           // add who is sending
-  return data;
+export function setUser(u) {
+  localStorage.setItem(STORAGE_KEY, u);
 }
 
-/* When you send to Twilio later:
-fetch('/main', {method:'POST', headers:{'Content-Type':'application/x-www-form-urlencoded'},
-                body:new URLSearchParams(data) });
-*/
+export function getUser() {
+  return localStorage.getItem(STORAGE_KEY) || '';
+}
+
+export function collectForm(form) {
+  const formData = new FormData(form);
+  const data = {};
+
+  for (const [key, value] of formData.entries()) {
+    if (data.hasOwnProperty(key)) {
+      if (Array.isArray(data[key])) {
+        data[key].push(value);
+      } else {
+        data[key] = [data[key], value];
+      }
+    } else {
+      data[key] = value;
+    }
+  }
+
+  data.user = getUser();
+  return data;
+}
